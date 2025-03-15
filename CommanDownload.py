7a99d35d8732b09e1580c558263be3d1bcd5ebd0
YuJY <1368048755@qq.com>
2025-03-15 17:29:26 +0800

    CommanDownload.py
    下载模块

--- CommanDownload.py
+++ CommanDownload.py
@@ -1,60 +1,69 @@
-import tkinter
+import wx
+import os
 import requests
-from tkinter import messagebox,ttk
-
+from wx import MessageBox, ProgressDialog
+from winotify import Notification, audio
 def download_file(url, local_path):
     global dirs
-    # 发送GET请求
+    
     ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
     headers = {"user-agent": ua}
     try:
         response = requests.get(url, stream=True,headers=headers)
     except Exception as e:
-        tkinter.messagebox.showwarning(title="",message="获取链接码失败:\n"+str(e)+"\n请检查网络、链接是否正确")
-  
+        MessageBox(f"获取链接码失败:\n{str(e)}\n请检查网络、链接是否正确", "错误", wx.OK | wx.ICON_ERROR)
+        return
+
     
-    # 检查请求是否成功
     if response.status_code == 200:
-        # 打开本地文件，准备写入
-        root=tkinter.Tk()
-        root.geometry("500x300")
-        root.title("下载...")
-        content_size=0xffffff
+        content_size = 0xffffff
         try:
-            content_size = int(response.headers['content-length']) #下载文件总大小
+            content_size = int(response.headers['content-length'])
         except:
             print("文件大小未知")
-        all_size=int(content_size/(1024*8))
-        Progressbar0=tkinter.ttk.Progressbar(root,length=600,orient="horizontal",mode="determinate")
-        Progressbar0.pack()
-        title=tkinter.Label(root,text="正在下载"+local_path+"  关闭此窗口以结束任务")
-        title.pack()
-        Progressbar0["maximum"]=all_size
-        Progressbar0["value"]=0
-        root.update_idletasks()
-        wr=0
+        
+       
+        progress = ProgressDialog("下载中", f"正在下载 {local_path}", maximum=content_size,
+                                style=wx.PD_APP_MODAL | wx.PD_AUTO_HIDE | wx.PD_CAN_ABORT)
+        
+        wr = 0
         try:
             with open(local_path, 'wb') as f:
-            
                 for chunk in response.iter_content(chunk_size=1024*8):
                     if chunk:
-                        wr+=1
-                        Progressbar0["value"]+=1
-                        
-                        root.update_idletasks()
-                        root.update()
+                        wr += len(chunk)
+                        (keepGoing, skip) = progress.Update(wr)
+                        if not keepGoing:
+                            break
                         f.write(chunk)
-                        
-            print("文件下载成功，保存在：", local_path)
             
-            root.destroy()
+            print("文件下载成功，保存在：", local_path)
+            progress.Destroy()
+            toast = Notification(
+                app_id="Advanced Network Toolset",
+                title="下载完成",
+                msg=f"文件已保存到：{local_path}",
+                duration="long"
+            )
+            toast.set_audio(audio.Default, loop=False)
+            
+            toast.show()
+
+           
+            dlg = wx.MessageDialog(None, f"文件已保存到：\n{local_path}", "下载完成", 
+                                 wx.OK | wx.CANCEL | wx.ICON_INFORMATION)
+            dlg.SetOKLabel("打开文件")
+            if dlg.ShowModal() == wx.ID_OK:
+                
+                os.startfile(local_path)
+            dlg.Destroy()
             return 0
         except Exception as e:
-            tkinter.messagebox.showwarning(title="",message="保存文件失败:\n"+str(e)+"\n请检查保存位置是否已满或有无权限")
+            MessageBox(f"保存文件失败:\n{str(e)}\n请检查保存位置是否已满或有无权限", "错误", wx.OK | wx.ICON_ERROR)
     else:
         print("文件下载失败，状态码：", response.status_code)
-        tkinter.messagebox.showerror(title="",message="错误:"+ str(response.status_code)+"请不要关闭下载界面")
-    
-    root.mainloop()
+        MessageBox( f"错误:{str(response.status_code)}请不要关闭下载界面", "错误", wx.OK | wx.ICON_ERROR)
+
 if __name__ == '__main__':
-    download_file("https://cdn.modrinth.com/data/o1tyE5vJ/versions/dmEZkFA7/custom-crosshair-mod-v1.5.2-forge-mc1.19.3.jar","test.png")
+    app = wx.App(False)
+    download_file("https://i-583.wwentua.com:446/03151600226787205bb/2025/03/08/1b619c17e8ff2f99d8ef3cbf09bfd53e.rar?st=uluRadQAcsvxuURYxHKE3A&e=1742030709&b=ARJdOQRyAGQHPAcyVmMEZAccCDFRJFcmBzsLdFwwAFZUaQlmBWkHcVNjVnQCDQUyA3gKYwYpATZUKQo0V34HdwEyXS8_c&fi=226787205&pid=183-94-62-137&up=2&mp=0&co=0","test.rar")
